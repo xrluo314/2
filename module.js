a49c8a128e846c186c1176e134778a2e8b383205
xrluo314 <50157026+xrluo314@users.noreply.github.com>
2019-09-15 15:18:30 -0400

    Update module.js

--- module.js
+++ module.js
@@ -1,15 +1,4 @@
-const express = require('express');
-var path = require("path");
-const app = express();
 
-app.use('/src', express.static(__dirname + '/src'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(8080, () => console.log('Listening on port 8080!'));
-express.static.mime.define({'application/javascript': ['js']});
 export function ceilling(x){
     return Math.ceil(x);
 }
